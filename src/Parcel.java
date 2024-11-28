2401c0702070d68e266681e8e689d372cd6435f3
Hardik Mathur <mathurhardik007@gmail.com>
2024-11-28 22:16:29 +0000

    Initial Project Creation

---
+++ src/Parcel.java
@@ -0,0 +1,29 @@
+
+
+public class Parcel {
+    private String parcelid;
+    private int days_in_depot;
+    private double weight;
+    private Dimensions dimensions;
+    private String Status;
+    private float discount;
+
+    public class Dimensions{
+        private float length;
+        private float width;
+        private float height;
+    }
+
+    //constructor
+    public Parcel(String ID,int days_in_depot,double weight,Dimensions dimensions,String Status,float discount){
+
+
+
+    }
+
+
+}
+
+
+
+
