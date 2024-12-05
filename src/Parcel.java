57c7274be29f0ee15081f4e987de518670ecf9b8
Hardik Mathur <mathurhardik007@gmail.com>
2024-12-05 14:14:03 +0000

    Parcel Class Created

--- src/Parcel.java
+++ src/Parcel.java
@@ -6,21 +6,50 @@ public class Parcel {
     private double weight;
     private Dimensions dimensions;
     private String Status;
-    private float discount;
+    private double discount;
+
 
     public class Dimensions{
-        private float length;
-        private float width;
-        private float height;
+        private double length;
+        private double width;
+        private double height;
+
+        public Dimensions(double length,double width,double height) {
+            this.length = length;
+            this.width=width;
+            this.height=height;
+        }
+
+        public double calculateVolume() {
+            return length * width * height;
+        }
     }
 
     //constructor
-    public Parcel(String ID,int days_in_depot,double weight,Dimensions dimensions,String Status,float discount){
-
-
-
+    public Parcel(String ID,int did,double weight,Dimensions dimensions,double discount){
+    this.parcelid=ID;
+    this.days_in_depot=did;
+    this.weight=weight;
+    this.dimensions=dimensions;
+    this.Status="waiting";
+    this.discount=discount;
     }
 
+    //method1-getID
+    public String getID() {
+        return parcelid;
+    }
+
+    //method2-daysindepot
+    public int getdays_in_depot() {return days_in_depot;}
+
+    //method3-dimensions
+    public double calculateVolume() {
+        return dimensions.calculateVolume();
+    }
+
+    //method4-weight
+    public double getWeight() {return weight;}
 
 }
 
